c90a923f743defb6aadb0a79665f7d5f715fa4dc
Masa Hassan Masri <93089580+masamasri01@users.noreply.github.com>
2023-07-31 03:50:44 +0300

    Add files via upload

---
+++ DigitalClock.X/LCD.c
@@ -0,0 +1,15 @@
+/*
+ * File:   LCD.c
+ * Author: masah
+ *
+ * Created on July 28, 2023, 11:40 AM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
---
+++ DigitalClock.X/build/default/production/Lcd.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/Lcd.d  \
+ build/default/production/Lcd.p1:  \
+ Lcd.c  \
+Lcd.h 
